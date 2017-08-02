a789db5269cb0179b530b7adaee8ae10cdfc2660
零度逍遥 <zhangxu@promni.cn>
2017-08-02 15:18:34 +0800

    微信精选列表

---
+++ controllers/wx.js
@@ -0,0 +1,78 @@
+const iconv = require('iconv-lite');
+const request = require('request');
+const cheerio = require("cheerio");
+
+const wx = {
+  list: list
+};
+
+function list (req, res) {
+  /**
+   * @type {string}
+   * 0热门、1推荐、2段子手、3养生堂、4私房话、5八卦精、6爱生活、7财经迷、
+   * 8汽车迷、9科技咖、10潮人帮、11辣妈帮、12点赞党、13旅行家、14职场人、
+   * 15美食家、16古今通、17学霸族、18星座控、19体育迷
+   *
+   */
+  let type = req.query.type || '0';
+  let page = req.query.page || '0';
+  let url = `http://weixin.sogou.com/wapindex/wap/0612/wap_${type}/${page}.html`;
+  let options = {
+    url: url,
+    encoding: null,
+    headers: {
+      'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1'
+    }
+  };
+
+  try {
+    request(options, function (err, _res, body) {
+      //先抓取页面，获取页面编码格式，然后再使用相应编码解析
+      let html = iconv.decode(body,'UTF8'); // gb2312;
+      try{
+        let str = html.split('charset=')[1];
+        let encode = str.match(/(\w+)\W/)[1].toUpperCase();
+        if (encode==='UTF') {
+          html = iconv.decode(body,'UTF8');
+        }else{
+          html = iconv.decode(body,encode);
+        }
+      }catch(e){
+        console.log('no charset');
+      }
+      //获取页面为元素
+      let $ = cheerio.load(html,{decodeEntities: false});
+      let aArticle = $('.list-txt');
+      let aPic = $('.pic');
+      let len = aArticle.length;
+      let list = [];
+      for (let i=0;i<len;i++) {
+        let oArticle = aArticle.eq(i);
+        let _obj = {
+          url: oArticle.find('a').attr('href'),
+          author: oArticle.find('.s2').text(),
+          timestamp: oArticle.find('.s3').data('lastmodified'),
+          pic: aPic.eq(i).find('img').attr('src')
+        };
+        list.push(_obj);
+      }
+
+      res.json ({
+        code: 10000,
+        errMsg: '',
+        data: list
+      })
+
+    });
+  }catch(e){
+    res.json ({
+      code: 102,
+      errMsg: '请求错误'
+    })
+  }
+
+
+
+}
+
+module.exports = wx;
